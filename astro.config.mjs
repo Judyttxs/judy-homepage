a7f6be7dfeeeac63d63ab96bbdcceb4dbbef5988
Judy <3089049566@qq.com>
2026-08-25 22:19:56 +0800

    feat: Judy 个人主页 v1.0

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  // 部署到自定义域名后，改为你自己的域名，用于生成 sitemap / canonical URL
+  site: 'https://judy666.icu',
+});
